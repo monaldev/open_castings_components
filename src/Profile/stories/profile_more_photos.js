94f3786bf3920c6adb59e6129475ff499dc55d47
Rohit Ravikoti <rohit@novvuminc.com>
2016-10-12 12:24:00 -0700

    fixed breaking changes, added faker

--- src/Profile/stories/profile_more_photos.js
+++ src/Profile/stories/profile_more_photos.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { ProfileMorePhotos } from '~/';
 import LoadMuiTheme from '~/.storybook/load_mui_theme.js';
+import faker from 'faker';
 
 storiesOf('ProfileMorePhotos', module)
   .addDecorator(story => (
@@ -22,16 +23,10 @@ storiesOf('ProfileMorePhotos', module)
       margin: '4px',
       backgroundColor: 'transparent',
     };
-    const data = [
-      'https://s2.graphiq.com/sites/default/files/stories/t2/tiny_cat_12573_8950.jpg',
-      'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg',
-      'https://www.petdrugsonline.co.uk/images/page-headers/cats-master-header',
-      'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg',
-      'https://s2.graphiq.com/sites/default/files/stories/t2/tiny_cat_12573_8950.jpg',
-      'https://www.petdrugsonline.co.uk/images/page-headers/cats-master-header',
-      'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg',
-      'https://www.petdrugsonline.co.uk/images/page-headers/cats-master-header',
-    ];
+    const data = [];
+    for (let i = 0; i < 8; i++) {
+      data.push(faker.random.image());
+    }
     const story = (
       <ProfileMorePhotos
         data={data}
